28575c570e78ffbeb704f37501c3f4289242f9e6
bsy3764 <bsy3764@gmail.com>
2021-10-23 22:55:39 +0900

    map(iteration) content add

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -8,9 +8,11 @@ const IterationSample = () => {
         { id: 4, text: '바람' }
     ]);
     const [inputText, setInputText] = useState('');
-    const [nextId, setNextId] = useState(5);
+    const [nextId, setNextId] = useState(5);    // 추가될 값이 5부터(기존에 4개)
     const onChange = e => setInputText(e.target.value);
     const onClick = () => {
+        // 상태를 업데이트할 때는 기존 상태를 그대로 두면서 새로운 값을 상태로 설정
+        // concat은 새로운 배열을 만들어 줌
         const nextNames = names.concat({
             id: nextId,
             text: inputText
